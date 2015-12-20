7a934ef479cdd1f906f10b1183e6a569274035c0
Erik Asland <erikasland87@gmail.com>
2015-12-20 11:20:37 -0800

    First working commit of game. Could use refactoring of code. Needs more relavant object names and could be a bit more object oriented. Also needs endpoint leader board. Basic functionality there though. Show players scores, uses ajax to submit all information to database, uses jquery to hide or show game html.

--- apps/tugowar/urls.py
+++ apps/tugowar/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import url
-from apps.tugowar.views import Index
+from apps.tugowar.views import Index, StartGame, EndGame
 
 urlpatterns = [
+    url(r'^endgame$', EndGame.as_view()),
+    url(r'^startgame$', StartGame.as_view()),
     url(r'^$', Index.as_view()),
 ]
--- tugowar/settings.py
+++ tugowar/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'tugowar.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'tugowar',
+        'USER': 'erik',
+        'PASSWORD': 'X0.v7RJ#!',
+        'HOST': 'localhost',
+        'POST': '',
     }
 }
 
